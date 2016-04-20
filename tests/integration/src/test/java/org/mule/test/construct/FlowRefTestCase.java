e648e61c29117e318064640f261d3fc6a274d789
Daniel Feist <dfeist@gmail.com>
2016-04-20 15:37:07 +0100

    MULE-9600 Remove unneeded processing strategies in mule 4

--- tests/integration/src/test/java/org/mule/test/construct/FlowRefTestCase.java
+++ tests/integration/src/test/java/org/mule/test/construct/FlowRefTestCase.java
@@ -182,16 +182,6 @@ public class FlowRefTestCase extends FunctionalTestCase
     }
 
     @Test
-    public void nonBlockingFlowRefToQueuedAsyncFlow() throws Exception
-    {
-        Response response = Request.Post(String.format("http://localhost:%s/%s", port.getNumber(), "nonBlockingFlowRefToQueuedAsyncFlow"))
-                .connectTimeout(RECEIVE_TIMEOUT).bodyString(TEST_MESSAGE, ContentType.TEXT_PLAIN).execute();
-        HttpResponse httpResponse = response.returnResponse();
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(500));
-        assertThat(IOUtils.toString(httpResponse.getEntity().getContent()), is("Unable to process a synchronous event asynchronously."));
-    }
-
-    @Test
     public void nonBlockingFlowRefToAsyncFlow() throws Exception
     {
         Response response = Request.Post(String.format("http://localhost:%s/%s", port.getNumber(), "nonBlockingFlowRefToAsyncFlow"))
